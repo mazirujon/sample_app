f13a309a5779ed4833f327b11273f712a73be88c
Truong Quang Hien <okabe.houtarou@gmail.com>
2019-05-16 02:15:41 +0700

    Add account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate(params[:session][:password])
-      log_in user
-      remember_check params[:session][:remember_me], user
-      redirect_back_or user
+      check_activated? user
     else
       flash.now[:danger] = t "messenger.invalid_mail"
       render :new
@@ -18,7 +16,22 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+  private
+
   def remember_check checked, user
     checked == Settings.CHECKED ? remember(user) : forget(user)
+  end
+
+  def check_activated? user
+    if user.activated?
+      log_in user
+      remember_check(params[:session][:remember_me], user)
+      redirect_back_or user
+    else
+      message = "Account not activated. "
+      message += "Check your email for the activation link."
+      flash[:warning] = message
+      redirect_to root_path
+    end
   end
 end
